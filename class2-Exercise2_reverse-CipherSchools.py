00389da254fc7c68ad254f5b3089c64b3a1cc626
Nishant singh <114793917+nishantsingh1107@users.noreply.github.com>
2022-11-29 16:53:29 +0530

    Update and rename class2-exercise2_revierse-cipherschools.py to class2-Exercise2_reverse-CipherSchools.py

---
+++ class2-Exercise2_reverse-CipherSchools.py
@@ -0,0 +1,5 @@
+# Problem- Ask user name and print back user name in reverse order.
+# Note- try to make your program in2 lines using string formatting.
+
+name = input("Enter your name : ")
+print(f"Your name in reverse order is {name[::-1]}")
